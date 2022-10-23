2eccafff32cd04128ac3757b9df1cba1dd6b8b7c
KatieRiches <103969925+KatieRiches@users.noreply.github.com>
2022-10-24 12:29:53 +1300

    Imported app from codesandbox

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import Weather from './weather';
 
-function App() {
+import "./App.css";
+import Weather from "./Weather";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         Weather app
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-        </a>
-  <Weather />
-  
-      </header>
+      <div className="container">
+        <Weather defaultCity="New York" />
+        <footer>
+          The project was cooded by Katie and is
+          <a
+            href="https://github.com/KatieRiches/Weather-app-react"
+          >
+            {" "}
+            open sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/weather.js
+++ src/weather.js
@@ -1,15 +1,68 @@
-import React from 'react';
-import axios from 'axios';
-import './App.css';
+import React, { useState } from "react";
+import "./Weather.css";
+import axios from "axios";
+import Weatherinfo from "./Weatherinfo";
 
-export default function Weather() {
+export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      date: new Date(response.data.dt * 1000),
+      temperature: response.data.main.temp,
+      humidity: response.data.main.humidity,
+      description: response.data.weather[0].description,
+      iconUrl: ``,
+      wind: response.data.wind.speed,
+      city: response.data.name,
+    });
+  }
 
-    function handleResponse(response){
-        alert(`The temperature in Cromwell is ${response.data.main.temp} °`)
-    }
-    let apiKey = `4cf67821f8a1ed58161e24f9867203f3`
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Cromwell&appid=${apiKey}&units=metric`;
-
+  function search() {
+    const apiKey = "2a54f8728b182fcfe9a83e02a050c48e";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
     axios.get(apiUrl).then(handleResponse);
-    return <h2>Hello from the weatherman</h2>;
+  }
+
+  function handleSubmit(event) {
+   event.preventDefault();
+   search();
+  }
+
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city..."
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+        <Weatherinfo data={weatherData}/>
+      </div>
+    );
+  } else {
+    search();
+    return "Loading...";
+  }
 }
